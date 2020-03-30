d4dbb27f7730d8a4d1d62332d8d455b7736eeb00
cnheider <mrnaah@gmail.com>
2020-03-30 10:28:05 +0200

    Change CNAME

--- heimdallr/board_layout/footer.py
+++ heimdallr/board_layout/footer.py
@@ -17,17 +17,15 @@ from heimdallr.utilities.heimdallr_config import (
 
 
 def get_footer():
-  return html.Footer(
-    [
-      html.Div(
-        [
-          html.P([f"{k}: "]),
-          html.A([html.Img(src=v)], href=v.split(".svg")[0]),
-          ],
-        className="col",
-        )
-      for k, v in BUILD_STATUS_MAPPING
-      ]
-    + [dcc.Interval(id=BUILD_STATUS_INTERVAL, interval=60 * 1000, n_intervals=0)],
-    className="page-footer text-center row p-3",
-    )
+  return html.Footer([
+                       html.Div([
+                         html.P([f"{k}: "]),
+                         html.A([html.Img(src=v)], href=v.split(".svg")[0]),
+                         ],
+                         className="col",
+                         )
+                       for k, v in BUILD_STATUS_MAPPING
+                       ]
+                     + [dcc.Interval(id=BUILD_STATUS_INTERVAL, interval=60 * 1000, n_intervals=0)],
+                     className="page-footer text-center row p-3",
+                     )
